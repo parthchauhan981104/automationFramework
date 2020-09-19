53797d20f260890dc9b6057a56572c513f33650a
Parth <gamerboy09pc@gmail.com>
2020-09-19 16:38:53 +0530

    Refactoring- Custom selenium driver class

--- base/selenium_driver.py
+++ base/selenium_driver.py
@@ -22,9 +22,9 @@ class SeleniumDriver():
             return By.XPATH
         elif locatorType == "css":
             return By.CSS_SELECTOR
-        elif locatorType == "classname":
+        elif locatorType == "class":
             return By.CLASS_NAME
-        elif locatorType == "linktext":
+        elif locatorType == "link":
             return By.LINK_TEXT
         else:
             print("Locator type " + locatorType + " not correct/supported")
@@ -36,9 +36,9 @@ class SeleniumDriver():
             locatorType = locatorType.lower()
             byType = self.getByType(locatorType)
             element = self.driver.find_element(byType, locator)
-            print("Element Found")
+            print("Element Found with locator: " + locator + " and  locatorType: " + locatorType)
         except:
-            print("Element not found")
+            print("Element not found with locator: " + locator + " and  locatorType: " + locatorType)
         return element
 
     def elementClick(self, locator, locatorType="id"):
@@ -50,9 +50,19 @@ class SeleniumDriver():
             print("Cannot click on the element with locator: " + locator + " locatorType: " + locatorType)
             print_stack()
 
-    def isElementPresent(self, locator, byType):
+    def sendKeys(self, data, locator, locatorType="id"):
         try:
-            element = self.driver.find_element(byType, locator)
+            element = self.getElement(locator, locatorType)
+            element.send_keys(data)
+            print("Sent data on element with locator: " + locator + " locatorType: " + locatorType)
+        except:
+            print("Cannot send data on the element with locator: " + locator +
+                  " locatorType: " + locatorType)
+            print_stack()
+
+    def isElementPresent(self, locator, locatorType="id"):
+        try:
+            element = self.getElement(locator, locatorType)
             if element is not None:
                 print("Element Found")
                 return True
